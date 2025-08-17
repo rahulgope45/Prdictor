c9130645afdb7b9cbf06bd3a8445e2f256172e4d
rahulgope45 <rahuleile456@gmail.com>
2025-08-17 21:10:51 +0530

    Backend giving responses

--- server/app.py
+++ server/app.py
@@ -1,1 +1,27 @@
-print('server')
+from fastapi import FastAPI
+import joblib
+import numpy as np
+
+app = FastAPI()
+
+#Loading Trained model 
+model = joblib.load("model.pkl")
+
+@app.get("/")
+def home():
+    return{"message": "Api is running"}
+
+@app.post("/predict")
+def predict(team1_strength: int, team2_strength: int):
+    data = np.array([[team1_strength,team2_strength]])
+    prediction = model.predict(data)[0]
+    probabilities =model.predict_proba(data)[0]
+
+
+    return{
+        "predicted_winner" : int(prediction),
+        "probabilities": {
+            "team1": probabilities[0],
+            "team2": probabilities[1]
+        }
+    }
